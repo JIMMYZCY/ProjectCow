31e2eb79fd542ceded3037e65a49f6875397d998
JIMMYZ <zcy1918755114@gmail.com>
2021-02-02 16:20:54 -0800

    Player Movement Test

---
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace JimmyTest
+{
+    public class PlayerMovementTest : NetworkBehaviour
+    {
+        [SerializeField]
+        public float moveSpeed = 10;
+
+        private void Update()
+        {
+            if(isLocalPlayer)
+            {
+                float moveHorizontal = Input.GetAxis("Horizontal");
+                float moveVertical = Input.GetAxis("Vertical");
+                Vector3 movement = new Vector3(moveHorizontal * moveSpeed, 0, moveVertical * moveSpeed);
+                transform.position = transform.position + movement;
+            }
+        }
+    }
+}
+
