1658c6966428d29cb50efcd1b04137ab7a36b0dc
JIMMYZ <zcy1918755114@gmail.com>
2021-02-03 20:30:53 -0800

    update playermovement

--- Assets/Scripts/PlayerMovementTest.cs
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-namespace JimmyTest
+public class PlayerMovementTest : NetworkBehaviour
 {
-    public class PlayerMovementTest : NetworkBehaviour
-    {
-        [SerializeField]
-        public float moveSpeed = 10;
+    [SerializeField]
+    public float moveSpeed = 10;
 
-        private void Update()
-        {
-            if(isLocalPlayer)
-            {
-                float moveHorizontal = Input.GetAxis("Horizontal");
-                float moveVertical = Input.GetAxis("Vertical");
-                Vector3 movement = new Vector3(moveHorizontal * moveSpeed, 0, moveVertical * moveSpeed);
-                transform.position = transform.position + movement;
-            }
-        }
+    private void Update()
+    {
+        if(!isLocalPlayer) { return; }
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal * moveSpeed, 0, moveVertical * moveSpeed);
+        transform.position = transform.position + movement;
     }
 }
+
 
