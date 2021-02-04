da2742a6eeea5290bb16d83f4620f73ea9da5961
JIMMYZ <zcy1918755114@gmail.com>
2021-02-03 23:08:00 -0800

    sync dead

--- Assets/Scripts/PlayerMovementTest.cs
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -3,23 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-namespace JimmyTest
+public class PlayerMovementTest : NetworkBehaviour
 {
-    public class PlayerMovementTest : NetworkBehaviour
-    {
-        [SerializeField]
-        public float moveSpeed = 10;
+    public float moveSpeed = 10;
 
-        private void Update()
+    [SyncVar]
+    public bool isDead = false;
+
+    [SyncVar]
+    public bool isRespawned = false;
+
+    public MeshRenderer player_MeshRenderer;
+    public MeshRenderer DeadBody_MeshRenderer;
+    public GameObject Respawn_Point;
+    public GameObject Deadbody;
+
+    private void Update()
+    {
+        if (isDead == false)
         {
-            if(isLocalPlayer)
-            {
-                float moveHorizontal = Input.GetAxis("Horizontal");
-                float moveVertical = Input.GetAxis("Vertical");
-                Vector3 movement = new Vector3(moveHorizontal * moveSpeed, 0, moveVertical * moveSpeed);
-                transform.position = transform.position + movement;
-            }
+            PlayerMove();
+        }
+
+        if (isDead == true)
+        {
+            PlayerDead();
+        }
+
+        if (isRespawned == true && isDead == true)
+        {
+            PlayerRespawn();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Killer")
+        {
+            isDead = true;
+        }
+    }
+
+    private void PlayerMove()
+    {
+        if (!isLocalPlayer) return;
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal * moveSpeed, 0, moveVertical * moveSpeed);
+        transform.position = transform.position + movement;
+    }
+
+
+    private void PlayerDead()
+    {
+        if (!isDead) return;
+
+        player_MeshRenderer.enabled = false;
+        DeadBody_MeshRenderer.enabled = true;
+    }
+    
+
+    private void PlayerRespawn()
+    {
+        if (isRespawned == true && isDead == true)
+        {
+            isDead = false;
+            isRespawned = false;
+            gameObject.transform.position = Respawn_Point.transform.position;
+            player_MeshRenderer.enabled = true;
+            DeadBody_MeshRenderer.enabled = false;
         }
     }
 }
+
 
