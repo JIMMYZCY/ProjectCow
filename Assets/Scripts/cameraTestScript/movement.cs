a60a94a009c6e58f7a0a649e3836d5feaed44695
Aman-0620 <Aman-0620@users.noreply.github.com>
2021-02-25 21:55:44 +0800

    Camera Added
    In the scene called "sandboxCamera", the camera can move with the mouse now.

---
+++ Assets/Scripts/cameraTestScript/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    float speed = 10.0f;
+    float rotationspeed = 100.0f;
+    //public GameObject player_prefab;
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical") * speed;
+        float horizontal = Input.GetAxis("Horizontal") * rotationspeed;
+
+        vertical *= Time.deltaTime;
+        horizontal *= Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+        transform.Rotate(0, horizontal, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "target")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/cameraTestScript/playermanager.cs
@@ -0,0 +1,35 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermanager : NetworkBehaviour
+{
+    //public string pname = "player";
+
+    /*void OnGUI()
+    {
+        name = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), pname);
+
+    }*/
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<movement>().enabled = true;
+            /*Camera.main.transform.position = this.transform.position - this.transform.forward * 8f + this.transform.up * 5f;
+            Camera.main.transform.LookAt(this.transform.position);
+            Camera.main.transform.parent = this.transform;*/
+        }
+    }
+    /*void Update()
+    {
+        if (isLocalPlayer)
+        {
+            this.GetComponentInChildren<TextMesh>().text = pname;
+        }
+    }*/
+}
